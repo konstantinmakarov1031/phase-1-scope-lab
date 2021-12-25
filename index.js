8c9ba663dbd0a0eb77afa4fcf58b4e1295c96a11
konstantin <makarovestates@gmail.com>
2021-12-24 21:16:17 -0600

    test attempt

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    var capitalized = customerName.toUpperCase()
+    return capitalized;
+}
+
+upperCaseCustomerName()
